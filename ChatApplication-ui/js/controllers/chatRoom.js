cffc5d966e1450ce4da14f279e11cd47c95a54b4
Niharika <niharika.gupta@snapdeal.com>
2017-02-21 03:13:54 +0530

    pubnub api integration

--- ChatApplication-ui/js/controllers/chatRoom.js
+++ ChatApplication-ui/js/controllers/chatRoom.js
@@ -1,8 +1,8 @@
 (function() {
     angular
         .module("WebChatApp")
-        .controller("chatRoomCtrl", ['$scope', 'network_service', '$rootScope', '$http', '$location', '$routeParams',
-            function($scope, network_service, $RS, $http, $location, $routeParams) {
+        .controller("chatRoomCtrl", ['$scope', 'network_service', '$rootScope', '$http', '$location', '$routeParams', 'Pubnub',
+            function($scope, network_service, $RS, $http, $location, $routeParams, Pubnub) {
 
                 $scope.channel = 'niki.ai';
                 $scope.userName = $RS.user;
@@ -31,8 +31,20 @@
                 $scope.startPrivateChat = function(user) {
                     $scope.chats = [];
 
+                    
+
+                    if ($scope.userName > user.emailId) {
+                        $scope.channel = user.emailId + $scope.userName;
+                    } else {
+                        $scope.channel = $scope.userName + user.emailId;
+                    }
+
+                    Pubnub.subscribe({
+                        channels: [$scope.channel]
+                    });
+
                     $scope.isPrivate = true;
-                    $scope.chats.push(user);
+                    // $scope.chats.push(user);
                     $scope.receiver = user;
                     //retrieve prev messages
                     var data = {
@@ -55,7 +67,9 @@
                 $scope.removeChat = function() {
                     $scope.numberOfCoversation = 0;
                     $scope.receiver = null;
-                    Pubnub.ngUnsubscribe({channel:$scope.channel});
+                    Pubnub.unsubscribe({
+                        channels: [$scope.channel]
+                    });
                 }
 
                 $scope.chooseChat = function(id) {
@@ -70,30 +84,27 @@
                     if (!sendMessage || sendMessage === '') {
                         return;
                     }
-                    /*PUBNUB.publish({
-                        channel: $scope.channel,
-                        message: {
-                            content: sendMessage,
-                            sender_uuid: $scope.uuid,
-                            date: new Date()
-                        },
-                        callback: function(m) {
-                            console.log(m);
-                            console.log("message Sent");
-                        }
-                    });*/
-/*
-                    Pubnub.ngPublish({
-                        channel: $scope.channel,
-                        message: sendMessage
-                    });
-*/
 
                     var params = {
                         message: sendMessage,
                         sender: $scope.userName,
                         receiver: sendTo.emailId
                     };
+
+
+                    Pubnub.publish({
+                            message: params,
+                            channel: $scope.channel
+                        },
+                        function(status, response) {
+                            if (status.error) {
+                                // handle error
+                                console.log("Error occured: " + status)
+                            } else {
+                                console.log("message Published w/ timetoken", response.timetoken)
+                            }
+                        }
+                    );
                     network_service.GET({
                         url: 'saveMessage',
                         params: params
@@ -101,19 +112,24 @@
                     }).then(function(response) {
                         if (response.status === 200) {
                             $scope.message = null;
+                            //pushing message in senders window
                             $scope.currentConversation.push(params);
                         }
-                    })
+                    });
 
                 };
 
                 $scope.splitString = function(input, splitChar, splitIndex) {
-                    // do some bounds checking here to ensure it has that index
+                    //if splitChar is not in input
+                    if (input.indexOf(splitChar) == -1) {
+                        return input;
+                    }
                     return input.split(splitChar)[splitIndex];
                 }
 
                 var init = function() {
 
+                    // if not able to get current user
                     var flag = 0;
                     for (var key in $scope.userName) {
                         if (hasOwnProperty.call($scope.userName, key)) {
@@ -132,104 +148,44 @@
                         formPOST: true
 
                     }).then(function(response) {
-                        //console.log(response.data.length);
                         $scope.allUsers = response.data;
                     });
-                    $scope.uuid = _.random(100).toString();
-                    /*var PUBNUB_demo = Pubnub.init({
-                        publish_key: 'pub-c-1fc67093-3d16-4e80-964a-1378a6b76c38',
-                        subscribe_key: 'sub-c-6e73ec4a-f5f4-11e6-ac91-02ee2ddab7fe',
-                        uuid: $scope.uuid
-                    });*/
-
-                    //receive messages
-
-                    /*
-                                        // Subscribing to the ‘messages-channel’ and trigering the message callback
-                                        PUBNUB.subscribe({
-                                            channel: $scope.channel,
-                                            // triggerEvents: ['callback'],
-                                            withPresence: true,
-                                            triggerEvents: ['message', 'presence', 'status'],
-                                            callback: function(message) {
-                                                console.log(message);
-                                            },
-                                            error: function(err) { console.log("Error in Sending Message: " + err); }
-                                        });*/
-
-                    /* // Listening to the callbacks
-                     $RS.$on(PUBNUB.getMessageEventNameFor($scope.channel), function(ngEvent, m) {
-                         $scope.$apply(function() {
-                             // $scope.messages.push(m)
-                             console.log(m);
-                         });
-                     });*/
-
-                    // $RS.$on(PUBNUB.getEventNameFor('publish', 'callback'),
-                    //     function(ngEvent, payload) {
-                    //         $scope.$apply(function() {
-                    //             console.log("here-----");
-                    //         });
-                    //     });
-                    // $RS.$on(PUBNUB.getPresenceEventNameFor($scope.selectedChannel), function(ngEvent, pnEvent, envelope, channel) {
-                    //     // apply presence event (join|leave) on users list
-                    //     handlePresenceEvent(pnEvent);
-                    // });
-                    /*$rootScope.$on(Pubnub.getMessageEventNameFor($scope.selectedChannel), function(ngEvent, envelope) {
-                        $scope.$apply(function() {
-                            // add message to the messages list
-                            $scope.chatMessages.unshift(envelope.message);
-                        });
-                    });*/
-
-                    /*// A function to display a nice uniq robot avatar 
-                    $scope.avatarUrl = function(uuid) {
-                        return 'http://robohash.org/' + uuid + '?set=set2&bgset=bg2&size=70x70';
-                    };*/
-
-                    // Pubnub.ngSubscribe({ channel: $scope.channel })
-                    // $RS.$on(Pubnub.ngMsgEv(theChannel), function(event, payload) {
-                    //     // payload contains message, channel, env...
-                    //     console.log('got a message event:', payload);
-                    // })
-                };
 
 
-                function publish() {
+                    Pubnub.init({
+                        subscribeKey: "sub-c-6e73ec4a-f5f4-11e6-ac91-02ee2ddab7fe",
+                        publishKey: "pub-c-1fc67093-3d16-4e80-964a-1378a6b76c38",
+                        ssl: true
+                    });
 
-                    pubnub = new PUBNUB({
-                        publishKey: 'pub-c-1fc67093-3d16-4e80-964a-1378a6b76c38',
-                        subscribeKey: 'sub-c-6e73ec4a-f5f4-11e6-ac91-02ee2ddab7fe'
-                    })
 
-                    function publishSampleMessage() {
-                        console.log("Since we're publishing on subscribe connectEvent, we're sure we'll receive the following publish.");
-                        var publishConfig = {
-                            channel: "hello_world",
-                            message: "Hello from PubNub Docs!"
-                        }
-                        pubnub.publish(publishConfig, function(status, response) {
-                            console.log(status, response);
-                        })
-                    }
-
-                    pubnub.addListener({
+                    // Subscribe to a channel
+                    Pubnub.addListener({
                         status: function(statusEvent) {
-                            if (statusEvent.category === "PNConnectedCategory") {
-                                publishSampleMessage();
+                            if (statusEvent.category === "PNUnknownCategory") {
+                                var newState = {
+                                    new: 'error'
+                                };
+                                Pubnub.setState({
+                                        state: newState
+                                    },
+                                    function(status) {
+                                        console.log(statusEvent.errorData.message)
+                                    }
+                                );
                             }
                         },
                         message: function(message) {
-                            console.log("New Message!!", message);
-                        },
-                        presence: function(presenceEvent) {
-                            // handle presence
+                            if (message.message.sender != $scope.userName) {
+                                $scope.currentConversation.push(message.message);
+                            }
+
                         }
                     })
-                    console.log("Subscribing..");
-                    pubnub.subscribe({
-                        channels: ['hello_world']
-                    });
+
+
+
+
                 };
 
                 init();
--- ChatApplication-ui/js/services/endpoint_service.js
+++ ChatApplication-ui/js/services/endpoint_service.js
@@ -16,8 +16,8 @@
 
                         //Production
                         // return "http://localhost:8080/WebChatApplication/";
-                        return "http://localhost:8080/WebChatApplication-0.0.1-SNAPSHOT/";
-                        // return "http://54.191.242.154:8080/WebChatApplication-0.0.1-SNAPSHOT/";
+                        // return "http://localhost:8080/WebChatApplication-0.0.1-SNAPSHOT/";
+                        return "http://54.191.242.154:8080/WebChatApplication-0.0.1-SNAPSHOT/";
                     }
                     return $location.protocol() + "://" + $location.host() + ":" + $location.port() + "/WebChatApplication/";
                 }
